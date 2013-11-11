48537ebaca89bae85d0e53749ce8c51ecdea82da
Eric Dobson <eric.n.dobson@gmail.com>
2013-11-10 22:57:10 -0800

    Cleanup comment.

--- pkgs/typed-racket-pkgs/typed-racket-lib/typed-racket/optimizer/unboxed-let.rkt
+++ pkgs/typed-racket-pkgs/typed-racket-lib/typed-racket/optimizer/unboxed-let.rkt
@@ -181,9 +181,7 @@
 ;; it's worth unboxing
 (define (could-be-unboxed-in? v exp)
 
-  ;; if v is a direct child of exp, that means it's used in a boxed
-  ;; fashion, and is not safe to unboxed
-  ;; if not, recur on the subforms
+  ;; Recur on the subforms
   (define (look-at exp)
     (ormap rec (syntax->list exp)))
 
