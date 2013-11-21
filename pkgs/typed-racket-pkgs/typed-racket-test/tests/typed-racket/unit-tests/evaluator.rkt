6d0fca7a7ee165ea95b6c89d5d0875a9b8127dd6
Eric Dobson <eric.n.dobson@gmail.com>
2013-11-21 08:31:09 -0800

    Working segfault.

--- pkgs/typed-racket-pkgs/typed-racket-test/tests/typed-racket/unit-tests/evaluator.rkt
+++ pkgs/typed-racket-pkgs/typed-racket-test/tests/typed-racket/unit-tests/evaluator.rkt
@@ -1,20 +1,26 @@
 #lang racket/base
 ;; Allow evaluation at phase1
 (require (for-syntax racket/base syntax/parse))
-(provide phase1-eval phase1-phase0-eval phase1-phase0-run)
+(provide phase1-eval phase1-phase0-eval phase1-phase0-run phase1-phase0-lambda)
 
-(define-namespace-anchor anchor)
-(define namespace (namespace-anchor->empty-namespace anchor))
+;(define-namespace-anchor anchor)
+;(define namespace (namespace-anchor->empty-namespace anchor))
 
 (define-syntax phase1-phase0-run
   (syntax-parser
     [(_ form:expr ...)
      #'(let-syntax ([go (lambda (stx) form ...)]) (go))]))
 
+(define-syntax phase1-phase0-lambda
+  (syntax-parser
+    [(_ args body:expr ...+)
+     #'(phase1-phase0-run #`#,(lambda args body ...))]))
+
+
 (define-syntax phase1-phase0-eval
   (syntax-parser
     [(_ form:expr ...)
-     #'(eval-syntax (quote-syntax (phase1-phase0-run form ...)) namespace)]))
+     #'((phase1-phase0-lambda () form ...))]))
 
 (define-syntax phase1-eval
   (syntax-parser
